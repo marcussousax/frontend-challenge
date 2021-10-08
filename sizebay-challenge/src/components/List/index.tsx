ca17c2ddfbc0d07371c16c7d6f279bde5cfeca1e
Marcus.Sousa <marvinsousa83@gmail.com>
2021-10-08 18:29:05 -0300

    WIP - Adding some initial interaction

--- sizebay-challenge/src/components/List/index.tsx
+++ sizebay-challenge/src/components/List/index.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 import Controls from './Controls'
+import { useStore } from '../../useTodoStore'
 
-const ITEMS = [
-  {
-    label: 'Drive a Cadillac across the Irish Sea',
-    status: 'todo',
-  },
-  {
-    label: 'Sell an elevator to Geronimo',
-    status: 'todo',
-  },
-]
+export interface ITodo {
+  _id: string
+  title: string
+  isCompleted: boolean
+  createAt: string
+}
 
 const List = () => {
+  // @ts-ignore
+  const todos = useStore((state) => state.todos)
+  debugger
   return (
     <>
       <Controls />
       <Ul>
-        {ITEMS.map((item) => (
-          <ListItem item={item} />
+        <Li style={{ padding: 0 }}>
+          <AddItem />
+        </Li>
+        {todos.map((item: ITodo) => (
+          <ListItem key={item._id} item={item} />
         ))}
       </Ul>
     </>
@@ -34,12 +37,49 @@ const Ul = styled.ul`
   margin: 0;
 `
 
+const AddItem = () => {
+  const [todoValue, setTodoValue] = React.useState('')
+
+  // @ts-ignore
+  const { addTodo } = useStore((state) => state)
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+
+    addTodo(todoValue)
+    setTodoValue('')
+  }
+  debugger
+  return (
+    <>
+      <AddItemInput
+        value={todoValue}
+        onChange={(e) => setTodoValue(e.target.value)}
+        type={'text'}
+        placeholder={'Add new item...'}
+      />
+      <button onClick={handleSubmit} type={'submit'}>
+        +
+      </button>
+    </>
+  )
+}
+const AddItemInput = styled.input`
+  flex: 1;
+  padding: 8px 16px;
+  background: #f4f4f4;
+  border: none;
+  overflow: hidden;
+  border-radius: 4px;
+  line-height: calc(40px - 18px);
+`
+
 const ListItem: React.FC<{
-  item: { label: string; status: string }
+  item: ITodo
 }> = ({ item }) => {
   return (
     <Li>
-      {item.label} - {item.status}
+      {item.title} - {item.isCompleted ? 'Done' : 'Todo'}
     </Li>
   )
 }
@@ -51,6 +91,7 @@ const Li = styled.li`
   border-radius: 4px;
   line-height: calc(40px - 18px);
   padding: 8px 16px;
+  display: flex;
 `
 
 export default List
